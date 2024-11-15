93fb4546f5abf80fe460c19c8a7e7e2a9d5d54c9
Johannes Choo <jhanschoo@gmail.com>
2024-11-15 21:40:46 +0800

    codecrafters submit [skip ci]

--- src/scanner.ml
+++ src/scanner.ml
@@ -4,16 +4,29 @@ let tokenize (file_contents : string) : Token.t list =
   let lineref = ref 0
   and posref = ref 0 in
   let f (acc : Token.t list) (c : char) : Token.t list =
-    let cstr = String.of_char c in
     posref := !posref + 1;
-    Token.(
-      let line = !lineref
-      and pos = !posref in
-      match c with
-      | '(' -> { tt = Left_Paren ; lexeme = cstr; line ; pos } :: acc
-      | ')' -> { tt = Right_Paren ; lexeme = cstr; line ; pos } :: acc
-      | '{' -> { tt = Left_Brace ; lexeme = cstr; line ; pos } :: acc
-      | '}' -> { tt = Right_Brace ; lexeme = cstr; line ; pos } :: acc
-      | _ -> acc) in
-  let rev_res = Token.{ tt = Eof ; lexeme = "" ; line = !lineref ; pos = !posref + 1 } :: String.fold file_contents ~init:[] ~f in
+    let line = !lineref
+    and pos = !posref in
+    let single_char_tmpl (tt : Token_type.t) : Token.t list =
+      { tt; lexeme = String.of_char c; line; pos } :: acc
+    in
+    match c with
+    | '(' -> single_char_tmpl Left_Paren
+    | ')' -> single_char_tmpl Right_Paren
+    | '{' -> single_char_tmpl Left_Brace
+    | '}' -> single_char_tmpl Right_Brace
+    | ',' -> single_char_tmpl Comma
+    | '.' -> single_char_tmpl Dot
+    | '-' -> single_char_tmpl Minus
+    | '+' -> single_char_tmpl Plus
+    | ';' -> single_char_tmpl Semicolon
+    | '*' -> single_char_tmpl Star
+    | '/' -> single_char_tmpl Slash
+    | _ -> acc
+  in
+  let rev_res =
+    Token.{ tt = Eof; lexeme = ""; line = !lineref; pos = !posref + 1 }
+    :: String.fold file_contents ~init:[] ~f
+  in
   List.rev rev_res
+;;
