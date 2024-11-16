cd5e2ae7256e5be0ad5dc5fb8c2e6c4a67b9fcb6
Johannes Choo <jhanschoo@gmail.com>
2024-11-16 20:14:29 +0800

    codecrafters submit [skip ci]

--- lib/run.ml
+++ lib/run.ml
@@ -1,16 +1,14 @@
 open Core
 
 let gobble = MenhirLib.Convert.Simplified.traditional2revised Parser.gobble
-
 let expression = MenhirLib.Convert.Simplified.traditional2revised Parser.expression
 
 let tokenize filename =
   let f (chan : In_channel.t) =
     let lexbuf = Sedlexing.Utf8.from_channel chan in
-    let (has_error, stream) = Scanner.filter_unknown ~print:true Scanner.tokenize lexbuf in
+    let has_error, stream = Scanner.filter_unknown ~print:true Scanner.tokenize lexbuf in
     let _ = gobble stream in
-    if !has_error
-    then exit 65
+    if !has_error then exit 65
   in
   In_channel.with_file filename ~f
 ;;
@@ -18,10 +16,15 @@ let tokenize filename =
 let parse filename =
   let f (chan : In_channel.t) =
     let lexbuf = Sedlexing.Utf8.from_channel chan in
-    let (has_error, stream) = Scanner.filter_unknown ~print:false Scanner.tokenize lexbuf in
-    let ast = expression stream in
-    print_endline (Ast.to_string ast);
+    let has_error, stream = Scanner.filter_unknown ~print:false Scanner.tokenize lexbuf in
     if !has_error
     then exit 65
+    else (
+      try
+        let ast = expression stream in
+        print_endline (Ast.to_string ast)
+      with
+      | Parser.Error -> exit 65)
   in
   In_channel.with_file filename ~f
+;;
